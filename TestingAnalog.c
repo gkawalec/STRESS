d0da6b4b8bd47851a4a9f70808179e63c8dc8b7f
klough <klough@gmx.com>
2016-11-01 16:06:31 -0600

    Create TestingAnalog.c
    Starting code to test the Analog signals from our sensors

---
+++ TestingAnalog.c
@@ -0,0 +1,41 @@
+// Code to test for inputs from HR or Accelerometer
+
+//Pin Names:
+int PinX = A2;
+int PinY = A1;
+int PinZ = A0;
+int HRPin2 = A3;
+int HRPin1 = A2;
+int HRPinRef = A1;
+
+//test values:
+int analog_test_x;
+int analog_test_y;
+int analog_test_z;
+int analog_test_output2;
+int analog_test_output1;
+int analog_test_reference;
+
+
+
+
+void setup() {
+    //Accelerometer:
+    pinMode(PinZ,INPUT); //z
+    pinMode(PinY,INPUT); //y
+    pinMode(PinX,INPUT); //x
+    //HeartRate: 
+    pinMode(HRPin2,INPUT); //Output 2 (Filtered More- if we so choose)
+    pinMode(HRPin1,INPUT); //Output 1 
+    pinMode(HRPinRef,INPUT); //Reference
+
+
+void loop() {
+    analog_test_x = analogRead(PinX);
+    analog_test_y = analogRead(PinY);
+    analog_test_z = analogRead(PinZ);
+    analog_test_output2 = analogRead(HRPin2);
+    analog_test_output1 = analogRead(HRPin1);
+    analog_test_reference = analogRead(HRPinRef);
+
+}
