605013af5cde3681e6359df47153386ebb702839
gkawalec <gk.awalec+git@gmail.com>
2016-11-03 10:13:26 -0600

    Update TestingAnalog.ino
    added a couple ways to blink the led

--- TestingAnalog.ino
+++ TestingAnalog.ino
@@ -1,5 +1,8 @@
 // Code to test for inputs from HR or Accelerometer
 
+void blink(void);
+bool blinking = 0;
+
 //Pin Names:
 int PinX = A2;
 int PinY = A1;
@@ -7,6 +10,7 @@ int PinZ = A0;
 int HRPin2 = A3;
 int HRPin1 = A2;
 int HRPinRef = A1;
+int led = D7; // builtin led light
 
 //test values:
 int analog_test_x;
@@ -28,14 +32,51 @@ void setup() {
     pinMode(HRPin2,INPUT); //Output 2 (Filtered More- if we so choose)
     pinMode(HRPin1,INPUT); //Output 1 
     pinMode(HRPinRef,INPUT); //Reference
+    
+    // LED
+    pinMode(led, OUTPUT);
+    //other way
+    //attachInterrupt(HRPin2, blink, RISING);
 }    
 
 void loop() {
+    
+    //Read
     analog_test_x = analogRead(PinX);
     analog_test_y = analogRead(PinY);
     analog_test_z = analogRead(PinZ);
+    
     analog_test_output2 = analogRead(HRPin2);
     analog_test_output1 = analogRead(HRPin1);
     analog_test_reference = analogRead(HRPinRef);
 
+    if ( analog_test_output2 > 2000) {
+        digitalWrite(led, HIGH);
+    }
+    if ( analog_test_output2 <= 2000) {
+        digitalWrite(led, LOW);
+    }
+    //pin_blink_led(HRPin2, 2000, led);
+    delayMicroseconds(50); // short to find peak
+    
+
+        
+    
+    
+}
+
+void pin_blink_led( PIN , VALUE, LED ,  )
+{
+    ( analogRead(PIN) >= VALUE ) ? digitalWrite(LED, HIGH) && delay(500) : digitalWrite(LED, LOW);
+}
+
+
+void blink ()
+{
+    if ( blinking == false ) {
+        blinking = true;
+        digitalWrite(led, HIGH);
+        delay(200);
+        digitalWrite(led, LOW);
+    }
 }
