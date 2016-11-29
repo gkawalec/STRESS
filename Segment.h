df48917f4a157f9db32c898d688afbb7b8e8fbee
gkawalec <gk.awalec+git@gmail.com>
2016-11-29 14:38:47 -0700

    Create Segment.h

---
+++ Segment.h
@@ -0,0 +1,21 @@
+#ifndef SEG_H
+#define SEG_H
+
+char accepted[34] = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F','G','H','I','J','L','N','O','P','R','S','T','U','W','Y','_','-','\'','^'};
+
+void display3 ( char symbols[3] ) 
+{
+    
+}
+
+void display1 ( char & symbol ) 
+{
+    
+}
+
+
+
+
+
+
+#endif
